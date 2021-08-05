17241d46172ca7769bd4857e001cfcfb17fdc5ae
Steven Sheehy <steven.sheehy@hedera.com>
2021-08-05 11:14:05 -0500

    first commit

---
+++ src/main/java/com/example/cockroachdbcopy/CockroachdbCopyApplication.java
@@ -0,0 +1,67 @@
+package com.example.cockroachdbcopy;
+
+import javax.sql.DataSource;
+
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.logging.Log;
+import org.postgresql.PGConnection;
+import org.postgresql.copy.CopyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.Assert;
+
+
+@SpringBootApplication
+public class CockroachdbCopyApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CockroachdbCopyApplication.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CockroachdbCopyApplication.class, args);
+    }
+
+
+    @Bean
+    ApplicationRunner runner() {
+        return args -> {
+            String csv = "8a3d70a3-d70a-4000-8000-00000000001d,denver,Hannah,400 Broad St,0987654321\n";
+            csv += "9eb851eb-851e-4800-8000-00000000001e,denver,Carl,53 W 23rd St,5678901234\n";
+
+            copy("COPY users FROM STDIN DELIMITER ',' NULL ''", csv);
+            copy("COPY users FROM STDIN CSV", csv);
+        };
+    }
+
+    private void copy(String sql, String data) throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            truncate(connection);
+            LOGGER.info("Executing SQL: {}", sql);
+            LOGGER.info("Inserting data: {}", data);
+
+            CopyManager copyManager = connection.unwrap(PGConnection.class).getCopyAPI();
+            long count = copyManager.copyIn(sql, new StringReader(data), 32768);
+            LOGGER.info("Inserted {} rows", count);
+            Assert.state(count == 2, "Expected 2 rows to be inserted but was actually " + count);
+        }
+    }
+
+    private void truncate(Connection connection) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("truncate table users cascade")) {
+            statement.executeUpdate();
+        }
+    }
+}
---
+++ src/test/java/com/example/cockroachdbcopy/CockroachdbCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cockroachdbcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockroachdbCopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
